4e0199b2bbd1940ef6e7d6e0e1d29de3b3e76027
Goel <Goel.Tushar@bcg.com>
2020-09-29 20:23:34 +0530

    route guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TblmovieComponent } from './tblmovie/tblmovie.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'tblmovie', component: TblmovieComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'tblmovie', component: TblmovieComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
