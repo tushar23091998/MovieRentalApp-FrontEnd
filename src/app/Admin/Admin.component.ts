f3b32df55b52899723b1d48c9d7ec6a067ce0e39
Goel <Goel.Tushar@bcg.com>
2020-10-08 00:04:38 +0530

    add movie done

--- src/app/Admin/Admin.component.ts
+++ src/app/Admin/Admin.component.ts
@@ -11,18 +11,33 @@ import { UserService } from '../_services/user.service';
   styleUrls: ['./Admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
+  model :any = {}
+  addMode : any = true;
+  deleteMode:any;
   constructor(private http:HttpClient, private userService: UserService, 
     private alertify: AlertifyService,private route: ActivatedRoute) { }
 
   ngOnInit() {
-
-    
+  }
+  deleteToggle(){
+    this.addMode = false;
+    this.deleteMode =true;
   }
 
-  showUsers()
+  addToggle()
   {
+    this.addMode = true;
+    this.deleteMode =false;
+  }
 
+  addMovie(model :any){
+    this.http.post('http://localhost:5000/api/movies',model).subscribe(() => {
+          this.alertify.success('Movie added successfully');
+        }, error => {
+          console.log(error);
+          this.alertify.error(error);
+      });
+      
   }
 
 
